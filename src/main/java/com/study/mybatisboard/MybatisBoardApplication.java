9c5c0f8e3dacd59233cc0e756fe3eeb34756d727
seonghyeon <dolla_@naver.com>
2022-07-26 09:14:10 +0900

    first commit

---
+++ src/main/java/com/study/mybatisboard/MybatisBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.mybatisboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/mybatisboard/MybatisBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mybatisboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
